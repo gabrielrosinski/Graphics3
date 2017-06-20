7ec27e7c509a553be38086902de4dcf47a3ee96b
adi5785905 <adi5785905@gmail.com>
2017-06-20 10:32:26 +0300

    why the f*ck this stupid motherF**king rotate wont work???

--- Graphics3/Graphics3/3dDrawing.cs
+++ Graphics3/Graphics3/3dDrawing.cs
@@ -227,22 +227,22 @@ namespace Graphics3
 
         public static void rotateZAxis(Form1 form, List<Polygon> polygonList, double angle)
         {
-            getCenter(form);
+           // getCenter(form);
             //    //get the angle in radians
-                double angleInRad = angle * (180.0 / Math.PI);
+                double angleInRad = angle / 180.0 * Math.PI;
             //    //pre calculate the cos and sin
             double cos = Math.Cos(angleInRad);
             double sin = Math.Sin(angleInRad);
-            form.centerPoint.z = form.centerPoint.y * cos - ((form.centerPoint.z * sin) + form.centerPoint.z);
-            form.centerPoint.y = form.centerPoint.y * sin + ((form.centerPoint.z * cos) + form.centerPoint.y);
-            form.centerPoint.x = form.centerPoint.z * sin + ((form.centerPoint.x * cos) + form.centerPoint.x);
+           
+            form.centerPoint.y = ((form.centerPoint.x - form.centerPoint.x) * sin + ((form.centerPoint.y - form.centerPoint.y) * cos) + form.centerPoint.y);
+            form.centerPoint.x = ((form.centerPoint.x - form.centerPoint.x) * cos - ((form.centerPoint.y - form.centerPoint.y) * sin) + form.centerPoint.x);
             foreach (Polygon polygon in polygonList)
             {
                 for (int i = 0; i < polygon.polygonPoints.Length; ++i)
                 {
                     Point3D point3D = polygon.polygonPoints[i];
-                    point3D.x = (((point3D.x- form.centerPoint.x) * cos) - ((point3D.y- form.centerPoint.y) * sin)+ form.centerPoint.x);
-                    point3D.y = (((point3D.x- form.centerPoint.x) * sin) + ((point3D.y- form.centerPoint.y) * cos)+ form.centerPoint.y);
+                    point3D.x = ((((point3D.x- form.centerPoint.x) * cos) - (point3D.y- form.centerPoint.y) * sin)+ form.centerPoint.x);
+                    point3D.y = ((((point3D.x- form.centerPoint.x) * sin) + (point3D.y- form.centerPoint.y) * cos)+ form.centerPoint.y);
 
                     polygon.polygonPoints[i] = point3D;
                 }
@@ -253,15 +253,14 @@ namespace Graphics3
 
         public static void rotateYAxis(Form1 form, List<Polygon> polygonList, double angle)
         {
-            getCenter(form);
+           // getCenter(form);
             //    //get the angle in radians
-            double angleInRad = angle * (180.0 / Math.PI);
+            double angleInRad = angle / 180.0 * Math.PI;
             //    //pre calculate the cos and sin
             double cos = Math.Cos(angleInRad);
             double sin = Math.Sin(angleInRad);
 
-            form.centerPoint.z = form.centerPoint.y * cos - ((form.centerPoint.z * sin) + form.centerPoint.z);
-            form.centerPoint.y = form.centerPoint.y * sin + ((form.centerPoint.z * cos) + form.centerPoint.y);
+            form.centerPoint.z = form.centerPoint.z * cos - ((form.centerPoint.x * sin) + form.centerPoint.x);
             form.centerPoint.x = form.centerPoint.z * sin + ((form.centerPoint.x * cos) + form.centerPoint.x);
             foreach (Polygon polygon in polygonList)
             {
@@ -280,22 +279,22 @@ namespace Graphics3
 
         public static void rotateXAxis(Form1 form, List<Polygon> polygonList, double angle)
         {
-            getCenter(form);
+           // getCenter(form);
             //    //get the angle in radians
-            double angleInRad = angle * (180.0 / Math.PI);
+            double angleInRad = angle / 180.0 * Math.PI;
             //    //pre calculate the cos and sin
             double cos = Math.Cos(angleInRad);
             double sin = Math.Sin(angleInRad);
-            form.centerPoint.z = form.centerPoint.y * cos - ((form.centerPoint.z * sin) + form.centerPoint.z);
-            form.centerPoint.y = form.centerPoint.y * sin + ((form.centerPoint.z * cos) + form.centerPoint.y);
-            form.centerPoint.x = form.centerPoint.z * sin + ((form.centerPoint.x * cos) + form.centerPoint.x);
+
+            form.centerPoint.z = (form.centerPoint.y * cos - (form.centerPoint.z * sin) + form.centerPoint.z);
+            form.centerPoint.y = (form.centerPoint.y * sin + (form.centerPoint.z * cos) + form.centerPoint.y);
             foreach (Polygon polygon in polygonList)
             {
                 for (int i = 0; i < polygon.polygonPoints.Length; ++i)
                 {
                     Point3D point3D = polygon.polygonPoints[i];
-                    point3D.z = ((point3D.y - form.centerPoint.y) * cos) - (((point3D.z - form.centerPoint.z) * sin) + form.centerPoint.z);
-                    point3D.y = ((point3D.y - form.centerPoint.y) * sin) + (((point3D.z - form.centerPoint.z) * cos)+ form.centerPoint.y);
+                    point3D.z = (((point3D.y - form.centerPoint.y) * cos) - ((point3D.z - form.centerPoint.z) * sin) + form.centerPoint.z);
+                    point3D.y = (((point3D.y - form.centerPoint.y) * sin) + ((point3D.z - form.centerPoint.z) * cos) + form.centerPoint.y);
 
                     polygon.polygonPoints[i] = point3D;
                 }
